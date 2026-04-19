1ff993de5f5e2695faff94bc792824ab07fb2454
DinoDude <pday32@gmail.com>
2026-04-19 18:38:33 +0100

    removed debug from leeds tab

--- football-stats/tabs/premier-league/2025-2026/leeds.php
+++ football-stats/tabs/premier-league/2025-2026/leeds.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 // LEEDS UNITED SURVIVAL TRACKER - Data queries
 
 $tableView = football_stats_get_table_view($db, 'PL', 'league_table_PL', $currentMainTab ?? '2025-2026');
