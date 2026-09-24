963435b0dba0c063c505def179b89b0dc62b4c23
CleverDino <pday32@gmail.com>
2026-09-24 11:23:50 +0100

    Add side-by-side season comparison tabs

---
+++ football-stats/tabs/championship/2025-2026/compare-seasons.php
@@ -0,0 +1,9 @@
+<?php
+$comparisonLeague = [
+    'code' => 'ELC',
+    'name' => 'Championship',
+    'live_table' => 'league_table_ELC',
+    'total_games' => 46,
+    'halfway_games' => 23,
+];
+require dirname(__DIR__, 3) . '/includes/season-comparison.php';
---
+++ football-stats/tabs/league-two/2025-2026/compare-seasons.php
@@ -0,0 +1,9 @@
+<?php
+$comparisonLeague = [
+    'code' => 'L2',
+    'name' => 'League Two',
+    'live_table' => 'league_table_L2',
+    'total_games' => 46,
+    'halfway_games' => 23,
+];
+require dirname(__DIR__, 3) . '/includes/season-comparison.php';
